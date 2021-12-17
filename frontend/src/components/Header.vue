3a142ddb603a329ef1e923717d251ae0ad899cfb
yangcxPusto <619980817@qq.com>
2021-12-17 18:43:27 +0000

    frontend some deletion and tiny change, before using element plus

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+    <div style="width: 200px">houtai</div>
+    <div style="flex: 1"></div>
+    <div style="width: 100px">xiala</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
